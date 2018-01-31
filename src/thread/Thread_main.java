83040cb7dbab68078bc227078202baf93030d15c
anna <anna@aaa>
2018-01-31 17:21:19 +0900

    thread

---
+++ src/thread/Thread_main.java
@@ -0,0 +1,34 @@
+package thread;
+
+public class Thread_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread_run[]th = new Thread_run[5];
+		Thread_runnable[]th2 = new Thread_runnable[5];
+		for(int i = 0; i < 4; i++) {
+			th[i] = new Thread_run();
+			th[i].start();
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			th2[i] = new Thread_runnable();
+			th2[i].run();
+		}
+		
+		for(int j = 0; j < 4; j++) {
+			try {
+				th[j].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for(int i=0; i<10; i++) {
+			System.out.println("main‚©‚ço—Í: "+i);
+			
+		}
+	}
+
+}
